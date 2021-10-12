6c14b85ed09d469656f66e5642e81623445afcca
Dario <dario.defrancisci92@gmail.com>
2021-10-12 15:20:25 +0200

    upgrade dischi

--- index.php
+++ index.php
@@ -8,7 +8,23 @@
     <title> PHP Dischi </title>
 </head>
 
+<?php 
+    include __DIR__ . '/database.php';
+        
+        foreach($db as $key => $poster) {
+            echo '<img>' . 'poster: ' . $poster['poster'] . ':'  . '</h5>';
+            echo '<h2>' . 'title: ' . $poster['title'] . ':'  . '</h2>';
+            echo '<h4>' . 'author: ' . $poster['author'] . ':'  . '</h4>';
+            echo '<h5>' . 'year: ' . $poster['year'] . ':'  . '</h5>';
+        }
+            
+
+?>
+
 <body>
+
+    
+
     <header>
         <div id="container-header">
             <div class="logoSpotify">
@@ -30,11 +46,15 @@
                         <h5> 1988 </h5>
                     </div>
                     <div class="cardDisc">
-                        <!-- <img src="" alt=""> -->
-                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="img">
-                        <h2> New Jersey </h2>
-                        <h4> Bon Jovi </h4>
-                        <h5> 1988 </h5>
+                        <?php 
+                            include __DIR__ . '/database.php';
+                                foreach($db as $key => $poster) {
+                                    echo '<img>' . $poster['poster'] ;
+                                    echo '<h2>' . 'title: ' . $poster['title'] . ':'  . '</h2>';
+                                    echo '<h4>' . 'author: ' . $poster['author'] . ':'  . '</h4>';
+                                    echo '<h5>' . 'year: ' . $poster['year'] . ':'  . '</h5>';
+                                }
+                            ?>
                     </div>
                 </div>
             </div>
