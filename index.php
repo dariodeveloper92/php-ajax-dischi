42cf906b167b70ee44ffbaadd39b9c5c4cbf31b9
Dario <dario.defrancisci92@gmail.com>
2021-10-12 14:12:46 +0200

    caricato struttura grafica

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title> PHP Dischi </title>
 </head>
+
 <body>
 
     <?php 
@@ -20,6 +21,25 @@
     ?>
     
     
-    <h1>ciao</h1>
+    <header>
+        <div id="container-header">
+            <div class="logoSpotify">
+                <a href="#">
+                <img src="./img/spotify-logo.svg" alt="Spotify logo">
+                </a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section id="container-main">
+            <div class="box-dischi">
+                <div class="disc">
+                    <div class="cardDisc">
+                        <!-- <img src="" alt=""> -->
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
